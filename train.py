6d3b48fd2accbd64914ed1a93067cab45b57283e
hajix <sergioajini@gmail.com>
2022-07-22 17:59:07 +0430

    Choose better hyper parameters

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ datamodule = LineDataModule(
     ]
 )
 datamodule.setup()
-model = CUDNNCTCLoss(
+model = NativeCTCLoss(
     datamodule.train_dataset.alphabet_list,
     datamodule.train_dataset.line_height,
 )
@@ -28,9 +28,10 @@ checkpoint_callback = ModelCheckpoint(
 lr_callback = LearningRateMonitor('step')
 trainer = Trainer(
     gpus=1,
-    max_epochs=1_000,
+    max_epochs=3,
     progress_bar_refresh_rate=10,
     callbacks=[checkpoint_callback, lr_callback],
+    gradient_clip_val=100,
     # precision=16,
 )
 trainer.fit(model, datamodule)
